6519ff4bbb68bec6b7d4bbb0123e2bab6a296fa5
SOUL X SYSTEM <154720925+mahtoanjali@users.noreply.github.com>
2024-01-06 20:17:44 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -1,6 +1,6 @@
 """
 Apache License 2.0
-Copyright (c) 2022 @PYRO_BOTZ
+Copyright (c) 2022 @QUEENx_GOD
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
